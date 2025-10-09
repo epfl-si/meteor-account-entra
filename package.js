85435dd522967d0eff57813248f82dd20f49223c
Julien Delasoie <julien.delasoie@epfl.ch>
2025-10-09 11:29:02 +0200

    Bump entra-oauth

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'epfl:accounts-entra',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Accounts connector for epfl:entra-oauth package, for Meteor 3+',
   git: 'https://github.com/epfl-si/meteor-account-entra',
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('3.3');
 
-  api.use('epfl:entra-oauth@0.0.6');
+  api.use('epfl:entra-oauth@0.0.7');
 
   api.use('ecmascript');
   api.use('accounts-oauth');
